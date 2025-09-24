6b5783191ad105fbc825a0446bbca301d85d9f4c
Cédric Maviet <cedric.maviet@gmail.com>
2025-09-24 15:36:05 +1100

    ajout de middleware et index.html ainsi que son css dans un dossier

---
+++ server-express.mjs
@@ -0,0 +1,29 @@
+import express from "express";
+import morgan from "morgan";
+
+const host = "localhost";
+const port = 8000;
+
+const app = express();
+
+if (app.get("env") === "development") app.use(morgan("dev"));
+
+app.use(express.static("static"));
+
+app.get("/random/:nb", async function (request, response, next) {
+  const length = request.params.nb;
+  const contents = Array.from({ length })
+    .map((_) => `<li>${Math.floor(100 * Math.random())}</li>`)
+    .join("\n");
+  return response.send(`<html><ul>${contents}</ul></html>`);
+});
+
+const server = app.listen(port, host);
+
+server.on("listening", () =>
+  console.info(
+    `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`,
+  ),
+);
+
+console.info(`File ${import.meta.url} executed.`);
