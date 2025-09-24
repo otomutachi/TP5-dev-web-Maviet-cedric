da6472815b340e1f82bcd3cb599fc618ff368da6
Cédric Maviet <cedric.maviet@gmail.com>
2025-09-24 17:10:42 +1100

    maj server-express

--- server-express.mjs
+++ server-express.mjs
@@ -1,10 +1,16 @@
 import express from "express";
 import morgan from "morgan";
+import createError from "http-errors";
+import logger from "loglevel";
 
 const host = "localhost";
 const port = 8000;
 
 const app = express();
+
+logger.setLevel(logger.levels.DEBUG);
+
+app.set("view engine", "ejs");
 
 if (app.get("env") === "development") app.use(morgan("dev"));
 
@@ -15,23 +21,48 @@ app.get("/random.html", async function (request, response, _next) {
   return response.send(`<html><ul><li>${randomNumber}</li></ul></html>`);
 });
 
-app.get("/random/:nb", async function (request, response, _next) {
+app.get("/random/:nb", async function (request, response, next) {
   const length = Number.parseInt(request.params.nb, 10);
   if (Number.isNaN(length)) {
-    return response.status(400).send("<html><body><p>400: Invalid number in request</p></body></html>");
+    return next(createError(400));
   }
-  const contents = Array.from({ length })
-    .map(() => `<li>${Math.floor(100 * Math.random())}</li>`)
-    .join("\n");
-  return response.send(`<html><ul>${contents}</ul></html>`);
+  const numbers = Array.from({ length }, () => Math.floor(100 * Math.random()));
+  const welcome = "Random Numbers";
+  return response.render("random", { numbers, welcome });
+});
+
+app.use((request, response, next) => {
+  logger.debug(`default route handler : ${request.url}`);
+  return next(createError(404));
+});
+
+app.use((error, _request, response, _next) => {
+  logger.debug(`default error handler: ${error}`);
+  const status = error.status ?? 500;
+  const stack = app.get("env") === "development" ? error.stack : "";
+  const result = { code: status, message: error.message, stack };
+  return response.render("error", result);
+});
+app.use((request, response, next) => {
+  console.debug(`default route handler : ${request.url}`);
+  return next(createError(404));
+});
+
+app.use((error, _request, response, _next) => {
+  console.debug(`default error handler: ${error}`);
+  const status = error.status ?? 500;
+  const stack = app.get("env") === "development" ? error.stack : "";
+  const result = { code: status, message: error.message, stack };
+  return response.render("error", result);
 });
 
 const server = app.listen(port, host);
 
 server.on("listening", () =>
-  console.info(
-    `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`,
-  ),
+  logger.info(
+    `HTTP listening on http://${server.address().address}:${server.address().port} with mode '${process.env.NODE_ENV}'`
+  )
 );
 
-console.info(`File ${import.meta.url} executed.`);
+logger.info(`File ${import.meta.url} executed.`);
+
